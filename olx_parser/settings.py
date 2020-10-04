aef92a9ca7de0815e5610573e3bb3a15d5ad82e8
VolodymyrBrintsov <Brintsov@gmail.com>
2020-10-04 11:43:53 +0200

    deploy with postgres

--- olx_parser/settings.py
+++ olx_parser/settings.py
@@ -78,15 +78,15 @@ WSGI_APPLICATION = 'olx_parser.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
- #       'NAME': BASE_DIR / 'db.sqlite3',
-#   }
-#}
-
 DATABASES = {
-    'default':  dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'olxscrapydatabase',
+        'USER': 'postgres',
+        'PASSWORD': '24122002djdf',
+        'HOST': 'localhost',
+        'POST': '5432',
+    }
 }
 
 
