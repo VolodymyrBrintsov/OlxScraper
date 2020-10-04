69a28e8810f151814dd0ea7d78eea6c6d0fb3302
VolodymyrBrintsov <Brintsov@gmail.com>
2020-10-04 13:17:10 +0200

    migrations

--- parse/models.py
+++ parse/models.py
@@ -8,7 +8,7 @@ class JobAdds(models.Model):
     name = models.CharField(max_length=50)
     user_since = models.CharField(max_length=150)
     price = models.CharField(max_length=150)
-    link = models.SlugField()
+    link = models.SlugField(max_length=255)
     time = models.CharField(default=datetime.datetime.now().strftime("%m/%d/%Y"), max_length=20)
 
     def __str__(self):
