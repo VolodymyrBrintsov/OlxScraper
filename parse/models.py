6743dcc7f8f56cf63f91ce028217ba3ea4f09a43
VolodymyrBrintsov <Brintsov@gmail.com>
2020-10-04 12:58:50 +0200

    heroku selenium deploy final

--- parse/models.py
+++ parse/models.py
@@ -3,7 +3,7 @@ import datetime
 
 class JobAdds(models.Model):
     title = models.TextField(default='')
-    phone = models.CharField(max_length=50)
+    phone = models.CharField(max_length=150)
     heading = models.CharField(max_length=50)
     name = models.CharField(max_length=15)
     user_since = models.CharField(max_length=50)
